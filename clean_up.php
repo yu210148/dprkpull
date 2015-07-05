329dde550b68ac7c2c6f5093c7d1961220cf75b5
Kevin Lucas <yu210148@gmail.com>
2015-07-05 07:57:55 -0400

    bugfix in clean-up 1

--- clean_up.php
+++ clean_up.php
@@ -2,7 +2,13 @@
 function clean_up(){
     $audioFileName = "KCNA_News-*.ogg";
     $command = "rm -rf $audioFileName";
-    $command = escapeshellcmd($command);
+    
+    // escaping the shell command was causing the wildcard not to work
+    //$command = escapeshellcmd($command);
+    
+    //debug
+    //$command = "echo $command";
+    
     exec($command);
     return 0;
 }
